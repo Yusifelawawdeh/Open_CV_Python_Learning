6ab7a1ac242caea37bd3e6691b5889b456241b87
yusife <yusif.elawawdeh@tamu.edu>
2022-06-07 12:35:52 -0500

    blur, dilation, erosion

--- Ch02/02_06 Begin/02_06.py
+++ Ch02/02_06 Begin/02_06.py
@@ -1,4 +1,21 @@
 import numpy as np
 import cv2
 
+#Gaussian blur operation
 image = cv2.imread("thresh.jpg")
+cv2.imshow("Original", image)
+blur = cv2.GaussianBlur(image, (5,55), 0)
+cv2.imshow("Blur", blur)
+
+#dil]ation and erosion aka inversion
+kernel = np.ones((5,5), 'uint8')
+
+dilate = cv2.dilate(image, kernel, iterations=1)
+erode = cv2.erode(image, kernel, iterations=1)
+
+cv2.imshow("Dilate", dilate)
+cv2.imshow("Erode", erode)
+
+#window operations to view the stuff
+cv2.waitKey(0)
+cv2.destroyAllWindows()
