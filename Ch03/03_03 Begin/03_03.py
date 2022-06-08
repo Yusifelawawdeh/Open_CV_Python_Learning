82c03b0c3419af94622394a49f63c2c1e3dd00b8
yusife <yusif.elawawdeh@tamu.edu>
2022-06-08 12:40:51 -0500

    adaptive thresholding for image segmentation explored this is the tech that flir was using in the tiny iphone thermal cams (cool i guess).

--- Ch03/03_03 Begin/03_03.py
+++ Ch03/03_03 Begin/03_03.py
@@ -1,2 +1,19 @@
 import numpy as np
 import cv2
+
+#adaptive thresholding for segmentation
+
+
+img = cv2.imread("sudoku.png", 0)
+cv2.imshow("Original", img)
+
+#regular thresholding here
+ret, thresh_basic = cv2.threshold(img,70,255, cv2.THRESH_BINARY)
+cv2.imshow("Basic Binary", thresh_basic)
+
+#adaptive here
+thresh_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+cv2.imshow("Adaptive", thresh_adapt)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
