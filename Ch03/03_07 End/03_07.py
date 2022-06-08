a63c7eacdee726df588c4004073b0989ae320ba7
yusife <yusif.elawawdeh@tamu.edu>
2022-06-08 15:48:29 -0500

    testing labels for contour detection

--- Ch03/03_07 End/03_07.py
+++ Ch03/03_07 End/03_07.py
@@ -6,7 +6,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 cv2.imshow("Binary", thresh)
 
-_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 img2 = img.copy()
 index = -1
